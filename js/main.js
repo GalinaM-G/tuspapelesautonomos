e106326a681680192ecd99383fb13c9b2d96daff
Galina <galina.mckoy@gmail.com>
2023-02-05 15:40:24 +0100

    add some hover effect on btns

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // select the target element
-const target = document.querySelector(".header__top");
+const headerScroll = document.querySelector(".header__top");
 
 // function to add the class when scroll position is reached
 function addClassOnScroll() {
     const scrollPosition = window.pageYOffset;
     if (scrollPosition > 20) {
-        target.classList.add("header__top--active");
+        headerScroll.classList.add("header__top--active");
     } else {
-        target.classList.remove("header__top--active");
+        headerScroll.classList.remove("header__top--active");
     }
 }
 
