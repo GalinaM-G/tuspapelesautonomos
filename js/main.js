99f23a9f3cba4d1e747560f7ec1c3a970af6fff8
Galina <galina.mckoy@gmail.com>
2023-02-08 15:50:37 +0100

    edits js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-// select the target element
+
 const headerScroll = document.querySelector(".header__top");
 
-// function to add the class when scroll position is reached
 function addClassOnScroll() {
     const scrollPosition = window.pageYOffset;
-    if (scrollPosition > 20) {
+    if (scrollPosition > 15) {
         headerScroll.classList.add("header__top--active");
     } else {
         headerScroll.classList.remove("header__top--active");
     }
 }
 
-// listen to the scroll event
 window.addEventListener("scroll", addClassOnScroll);
